55ff2e03e58a2fcac38cf1be5e9be17878356ca0
mohuabtisha <112000888+mohuabtisha@users.noreply.github.com>
2023-10-29 15:50:54 +0600

    Add files via upload

---
+++ radar_test1/radar_test1.ino
@@ -0,0 +1,77 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
+Servo myservo;
+
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const  int trig=A5;
+const  int echo=A4;
+float Time=0,distance;
+int value=0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(A0,INPUT);
+  pinMode(8,OUTPUT);
+  pinMode(A1,OUTPUT);
+  pinMode(A2,OUTPUT);
+  myservo.attach(11);
+  
+  lcd.begin(16, 2);
+  lcd.print("Arduino Radar.");
+  delay(2000);
+  
+}
+
+void loop() {
+   //sonar();
+
+   for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);// tell servo to go to position in variable 'pos'
+    sonar();
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    sonar();
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+   
+}
+
+void sonar ()
+{
+  int value = analogRead(A0);
+   distance = (2.00*value) / 10.00;
+
+   if(distance<50)
+   {
+    digitalWrite(8,1);
+    digitalWrite(A1,1);
+    digitalWrite(A2,0);
+    }
+   else if(distance<100)
+   {
+    digitalWrite(8,1);
+    digitalWrite(A1,0);
+    digitalWrite(A2,1);
+    }
+   else
+   {
+    digitalWrite(8,0);
+    digitalWrite(A1,0);
+    digitalWrite(A2,0);
+   }
+   
+
+   lcd.clear();
+   lcd.print(distance);
+   lcd.setCursor(6, 0);
+   lcd.print("inch");
+   delay(50);
+}
+
---
+++ radar_testfor hardware/radar_testf/radar_testf.ino
@@ -0,0 +1,165 @@
+#include <Servo.h>
+#include <LiquidCrystal.h>
+ 
+Servo myservo;
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+ 
+int pos = 0; // motor position initiating
+const int trigPin = 9;
+const int echoPin = 10;
+const int moteur = 11;
+const int buzzer = 12;
+const int ledPin1 = 14;
+const int ledPin2 = 15;
+float distanceCm, DistanceSec,duration;
+ 
+void setup() {
+myservo.attach(moteur); // servo attached to the pin 11 
+lcd.begin(16,2); // initialize lcd
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(buzzer, OUTPUT);
+pinMode(ledPin1, OUTPUT);
+pinMode(ledPin2, OUTPUT);
+DistanceSec=20;
+ 
+}
+ 
+void loop() {
+for (pos = 0; pos <= 180; pos += 1) { // servo angle setting 0 to 180 degre
+// in steps of 1 degree
+myservo.write(pos); // servo program
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH); //trigger delay
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+ 
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+if (distanceCm <= DistanceSec)
+{
+ 
+if(distanceCm <= DistanceSec/2)
+{
+ 
+tone(buzzer, 10); // Send 1KHz sound signal...
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, HIGH);
+delay(700);
+noTone(buzzer); // Stop sound...
+lcd.setCursor(0,0); // position lcd cursor to 0,0
+lcd.print("Distance: "); // Print "Distance" on LCD
+lcd.print(distanceCm); 
+lcd.print(" cm "); 
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Angle : ");
+lcd.print(pos);
+lcd.print(" deg ");
+delay(2000);
+}
+else
+{
+digitalWrite(buzzer, HIGH);
+digitalWrite(ledPin2, LOW);
+digitalWrite(ledPin1, HIGH);
+delay(100);
+digitalWrite(buzzer, LOW);
+lcd.setCursor(0,0); // position lcd cursor to 0,0
+lcd.print("Distance: "); // Print "Distance" on LCD
+lcd.print(distanceCm); 
+lcd.print(" cm ");
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Angle : ");
+lcd.print(pos);
+lcd.print(" deg ");
+delay(2000);
+}
+}
+else{
+digitalWrite(buzzer, LOW);
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, LOW);
+}
+ 
+lcd.setCursor(0,0); // position lcd cursor to 0,0
+lcd.print("Distance: "); // Print "Distance" on LCD
+lcd.print(distanceCm); 
+lcd.print(" cm "); 
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Angle : ");
+lcd.print(pos);
+lcd.print(" deg ");
+delay(80);
+ 
+}
+for (pos = 180; pos >= 0; pos -= 1) { //
+myservo.write(pos); //
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+ 
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+if (distanceCm <= DistanceSec){
+if(distanceCm <= DistanceSec/2)
+{
+tone(buzzer, 10); // Send 1KHz sound signal...
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, HIGH);
+delay(700);
+noTone(buzzer); // Stop sound...
+lcd.setCursor(0,0); // position lcd cursor to 0,0
+lcd.print("Distance: "); // Printe "Distance" on LCD
+lcd.print(distanceCm); 
+lcd.print(" cm "); 
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Angle : ");
+lcd.print(pos);
+lcd.print(" deg ");
+delay(2000);
+}
+else
+{
+digitalWrite(buzzer, HIGH);
+digitalWrite(ledPin2, LOW);
+digitalWrite(ledPin1, HIGH);
+delay(100);
+digitalWrite(buzzer, LOW);
+lcd.setCursor(0,0); // position lcd cursor to 0,0
+lcd.print("Distance: "); // Printe "Distance" on LCD
+lcd.print(distanceCm); 
+lcd.print(" cm "); 
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Angle : ");
+lcd.print(pos);
+lcd.print(" deg ");
+delay(2000);
+}
+}
+else{
+digitalWrite(buzzer, LOW);
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, LOW);
+}
+ 
+lcd.setCursor(0,0); //
+lcd.print("Distance: "); //
+lcd.print(distanceCm); //
+lcd.print(" cm ");
+delay(10);
+lcd.setCursor(0,1);
+lcd.print("Angle : ");
+lcd.print(pos);
+lcd.print(" deg ");
+delay(80);
+}
+}
+
